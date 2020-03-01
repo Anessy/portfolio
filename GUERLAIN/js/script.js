950bee50d64324359adedc03b0decc00f55f9222
Anessy <n.repekha@gmail.com>
2020-03-01 20:23:36 +0200

    video for mobile

--- GUERLAIN/js/script.js
+++ GUERLAIN/js/script.js
@@ -1,9 +1,22 @@
+if( window.innerWidth <= 767 ){
+  document.querySelector('#player').hidden = true;
+} else {
+  function second_passed() {
+    document.querySelector('#player').hidden = true;
+   }
+  
+  setTimeout(second_passed, 7500);
+}
+
 //setTimeout('alert("прошла секунда")', 10000)
 
 // первый аргумент - функция
- function second_passed() {
-  document.querySelector('#player').hidden = true;
- }
 
-setTimeout(second_passed, 7500);
+//  function second_passed() {
+//   document.querySelector('#player').hidden = true;
+//  }
+
+// setTimeout(second_passed, 7500);
+
+
 
