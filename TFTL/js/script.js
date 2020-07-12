65a84c1ed3ad992169027ab4ee19c112f703ca39
Anessy <n.repekha@gmail.com>
2020-07-12 10:20:52 +0300

    add new css to tftl

--- TFTL/js/script.js
+++ TFTL/js/script.js
@@ -4,10 +4,15 @@ if (document.documentElement.clientWidth < 577){
   const afterMenu = document.querySelector('.afterMenu');
   afterMenu.style.display = 'block';
 
-  
     afterMenu.addEventListener('click', () => {
     menu.style.display === 'block' ? menu.style.display = 'none' : menu.style.display = 'block';
   });
+
+  const jsGirl1 = document.querySelector('.js-girl1'); 
+  const jsGirl3 = document.querySelector('.js-girl3');
+  jsGirl1.style.opacity = '0';
+  jsGirl3.style.opacity = '0';
 };
+
     
   
